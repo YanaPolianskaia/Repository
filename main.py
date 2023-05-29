63167c91ff7dbce8fd5e7b22f1f4afff04d6869e
Yana Polianskaia <yanapolianskaiaa@gmail.com>
2023-05-29 16:11:46 +0300

    calculator

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 while (True):
-    x = int(input("введите первое число"))
-    y = int(input("введите второе число"))
-    summ = x+y
-    print(summ)
+    a = input("введите выражение").split()
+    x = int(a[0])
+    y = a[1]
+    z = int(a[2])
+    if y == "+": 
+        print(x+z)
+    elif y == "-":
+        print(x-z)
+    elif y == "*":
+        print(x*z)
+    elif y == "/":
+        print(x/z)
+
+
 
